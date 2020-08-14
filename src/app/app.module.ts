84a2576e2cd767b974a2e42bf5e63bee064e52e8
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-08-14 21:51:58 +0300

    Сделан шаблон превью поста, шаблон поста

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MainLayoutComponent } from './shared/components/main-layout/main-layout
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { LoginPageComponent } from './admin/login-page/login-page.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginPageComponent } from './admin/login-page/login-page.component';
     MainLayoutComponent,
     HomePageComponent,
     PostPageComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
