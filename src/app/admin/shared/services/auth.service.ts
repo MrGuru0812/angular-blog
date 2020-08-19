f7e8af555c15f7cb54214d2efc3190bf28c64c73
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-08-19 08:32:31 +0300

    Настройка firebase

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@angular/core";
 import {HttpClient} from '@angular/common/http';
 import { User } from 'src/app/shared/interfaces';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import {tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
@@ -14,8 +16,10 @@ export class AuthService {
 
     login(user: User): Observable<any> {
 
-        return  this.http.post('', user)
-
+        return  this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+        .pipe(
+            tap(this.setToken)
+        )
     }
 
     logout() {
@@ -26,8 +30,9 @@ export class AuthService {
         return !!this.token
     }
 
-    private setToken() {
-
+    private setToken(response) {
+        console.log(response);
+        
     }
 
 }
